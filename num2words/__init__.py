aa529c98bb31e4bffcbe580e9dbfa3f5718f19dd
Sujay S Kumar <sujay.skumar141295@gmail.com>
2019-04-04 16:31:36 +0530

    Added malayalam num2words support

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -21,7 +21,7 @@ from . import (lang_AR, lang_CZ, lang_DE, lang_DK, lang_EN, lang_EN_IN,
                lang_ES, lang_ES_CO, lang_ES_VE, lang_FI, lang_FR, lang_FR_BE,
                lang_FR_CH, lang_FR_DZ, lang_GU, lang_HE, lang_HI, lang_HI_EN,
                lang_ID, lang_IT, lang_JA, lang_KN, lang_KO, lang_LT, lang_LV,
-               lang_MR, lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR,
+               lang_ML, lang_MR, lang_NL, lang_NO, lang_PL, lang_PT, lang_PT_BR,
                lang_RO, lang_RU, lang_SL, lang_SR, lang_TA, lang_TE, lang_TH, lang_TR,
                lang_UK, lang_VI)
 
@@ -48,6 +48,7 @@ CONVERTER_CLASSES = {
     'ko': lang_KO.Num2Word_KO(),
     'lt': lang_LT.Num2Word_LT(),
     'lv': lang_LV.Num2Word_LV(),
+    'ml': lang_ML.Num2Word_ML(),
     'mr': lang_MR.Num2Word_MR(),
     'pl': lang_PL.Num2Word_PL(),
     'ro': lang_RO.Num2Word_RO(),
